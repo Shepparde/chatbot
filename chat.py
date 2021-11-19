889d991bda825414b8fd7cd71415132ce1df8fd0
Shepparde <cyuthen@gmail.com>
2021-11-19 16:46:54 +0100

    imports

--- chat.py
+++ chat.py
@@ -1,17 +1,13 @@
 import re
 import pandas
 from datetime import datetime
-<<<<<<< HEAD
 from sklearn.model_selection import train_test_split
-=======
->>>>>>> 1e8c266305911e78f80c4cdba2d799662e5d769c
 
 # Opening JSON file as dataframe
 df = pandas.read_csv('que-faire-a-paris-.csv', delimiter = ';')
 
 #cleaning description
 df = df[df['Date de fin'] > str(datetime.today().strftime('%Y-%m-%d'))]
-<<<<<<< HEAD
 description = df['Description'].apply(lambda x: re.sub('<[^<]+?>', ' ', x))
 
 #text analysis
@@ -33,6 +29,3 @@ stopwords = nlp.Defaults.stop_words
 
 training_sentences, test_sentences = train_test_split(df['Description'], test_size=0.8)
 
-=======
-df['Description'] = df['Description'].apply(lambda x: re.sub('<[^<]+?>', '', x))
->>>>>>> 1e8c266305911e78f80c4cdba2d799662e5d769c
