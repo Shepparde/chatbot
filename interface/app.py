e17c321421bab9880a5d6bb331e6ebc5e885cf6c
deoliveir <deoliveir@cy-tech.fr>
2021-11-30 21:35:54 +0100

    tentative pour importer chatbot dans flask

--- interface/app.py
+++ interface/app.py
@@ -1,7 +1,8 @@
 import random
-from flask import Flask, render_template, request
 import re
 
+from chatbot import chat
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,11 +15,12 @@ def home():
 #function for the bot response
 def get_bot_response():
     userText = request.args.get('msg')
+    
     salutationsAnswers = re.compile('bonjour|bjr|hello|hi')
     if re.search(salutationsAnswers, userText.lower()) and len(userText) < 10:
         return str("Bonjour, <br> pouvez-vous me décrire en détail vos envies d'activités")
     else:
-        return str("Je n'ai pas bien compris votre demande, pouvez-vous s'il vous plait me décrire en détail vos envies d'activités?")
+        return print(chat.chatbot(userText.lower()))#str("Je n'ai pas bien compris votre demande, <br>pouvez-vous s'il vous plait me décrire en détail vos envies d'activités?")
 
 if __name__ == "__main__":
     app.run()
